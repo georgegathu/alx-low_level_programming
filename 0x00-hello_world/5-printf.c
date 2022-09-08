dd7f64bb92328eeb9f874cf2e737a0d17aefd9a2
georgegathu <ggeorge.gathu@gmail.com>
2022-09-08 14:03:41 -0700

    quiz 7 and 8

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 
-/*
- * main - print the string in the put function
- *
- *this program prints "Programming is like building a multilingual puzzle,
- * Return: 0
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
 int main(void)
 {
