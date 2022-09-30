565dde1edf1198345b70dd6bf0e30620b476959e
georgegathu <ggeorge.gathu@gmail.com>
2022-10-01 00:23:18 +0300

    Static Library

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strbrk - search a string for any of a set of bytes
+ * @s: string to cheeck
+ * @accept: string to check against
+ *
+ * Return: pointer to byte in s that matches or NULL if no match
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int k;
+
+	while (*s)
+	{
+		for (k = 0; accept[k]; k++)
+		{
+			if (*s == accept[k])
+				return (s);
+		}
+		s++
+	}
+	return (0);
+}
